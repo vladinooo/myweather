ecfc9409e024a119cb33adc5369f9a4793717060
Vlad Hartmann <hartmannvlad@gmail.com>
2016-02-15 20:36:49 +0200

    Added the openweatermap API key.

--- app/components/forecast/services/weatherService.js
+++ app/components/forecast/services/weatherService.js
@@ -7,7 +7,7 @@ weatherApp.service('weatherService', ['$resource', function ($resource) {
         var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily",
             {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
 
-        return weatherAPI.get({q: city, cnt: days, units: 'metric'}, function() {
+        return weatherAPI.get({q: city, cnt: days, units: 'metric', APPID: 'd3f5fa5abb1f73fbd4d119d665ce986d'}, function() {
 
             customJs.closeModal();
 
